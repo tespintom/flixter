0c23eb47e8774b7ca77e2f075ffcb968185b7614
Tomas Espina <tespintom@icloud.com>
2017-09-08 19:51:12 +0000

    fixed navbar and added courses controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

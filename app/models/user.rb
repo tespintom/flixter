bbe2790bdadfbb7795180382f47b5d2b09364578
Tomas Espina <tespintom@icloud.com>
2017-09-26 02:01:34 +0000

    student enrollement page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,25 @@
 class User < ApplicationRecord
   has_many :courses
+  has_many :enrollments
+  has_many :enrolled_courses, through: :enrollments, source: :course
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # def enrolled_in?(course)
+  #   enrolled_courses = enrollments.collect do |enrollment|
+  #     enrollment.course
+  #   end
 
+  # return enrolled_courses.include?(course)
 
+  # def enrolled_in?(course)
+  #   enrolled_courses = enrollments.collect(&:course)
+  #   return enrolled_coures.include?(course)
+  # end
 
-
+  def enrolled_in?(course)
+    return enrolled_courses.include?(course)
+  end
 end

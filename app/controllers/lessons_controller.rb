ba2ab1ebdc9bd98b45e0d3921cef24987720cd43
Tomas Espina <tespintom@icloud.com>
2017-09-26 14:47:43 +0000

    added permision to enrollment

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_lesson
+    if !current_user.enrolled_in?(current_lesson.section.course) && current_lesson.section.course.user != current_user
+      redirect_to course_path(current_lesson.section.course), alert: 'You are not currently enrolled'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson

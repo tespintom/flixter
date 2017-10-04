dd209c97108d334eb2e43827a2601a9aac42c297
Tomas Espina <tespintom@icloud.com>
2017-10-04 20:04:08 +0000

    lesson fixed

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,7 +1,7 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_section, only: [:new, :create]
-  before_action :require_authorized_for_current_section, only: [:update]
+  before_action :require_authorized_for_current_lesson, only: [:update]
 
   def new
     @lesson = Lesson.new
@@ -14,18 +14,24 @@ class Instructor::LessonsController < ApplicationController
 
   def update
     current_lesson.update_attributes(lesson_params)
-    render_plain:'updated!'
+    render plain: 'updated!'
   end
 
   private
 
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
   def current_lesson
-    @current_lesson ||=Lesson.find(params[:id])
+    @current_lesson ||= Lesson.find(params[:id])
   end
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-       render plain: 'Unauthorized', status: :unauthorized
+      render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
@@ -33,13 +39,6 @@ class Instructor::LessonsController < ApplicationController
   def current_section
     @current_section ||= Section.find(params[:section_id])
   end
-
-  # ||= means if @current_section == nil
-  #   @current_section = Section.find(params[:section_id])
-  #   @current_section
-  # else
-  #   @current_section
-  # end
 
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
